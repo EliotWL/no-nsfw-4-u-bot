619b727b3dec0cf7cf87b090c884a508505de173
El <39184025+EliotWL@users.noreply.github.com>
2025-03-21 02:28:48 -0400

    Major Overhaul
    Added new settings, cleaned up the code a bunch, added defaults, changed some parts around

--- src/main.tsx
+++ src/main.tsx
@@ -12,21 +12,48 @@ Devvit.addSettings([
     type: 'string',
     name: 'postReportReason',
     label: 'What should the posts reported for this be reported as?',
+    onValidate: (event) => {
+        console.log(event)
+        if(event.value == "")
+        {
+            return 'Must be at least 1 character';
+        }
+    },
+    defaultValue: 'This user is a minor making a NSFW post',
+    helpText: 'Default: This user is a minor making a NSFW post'
 },
 {
     type: 'string',
     name: 'commentReportReason',
     label: 'What should the comments reported for this be reported as?',
+    onValidate: (event) => {
+        console.log(event)
+        if(event.value == "")
+        {
+            return 'Must be at least 1 character';
+        }
+    },
+    defaultValue: 'This user is a minor commenting on a NSFW post',
+    helpText: 'Default: This user is a minor commenting on a NSFW post'
 },
 {
     type: 'string',
     name: 'modnoteText',
     label: 'What should the mod note say?',
+    onValidate: (event) => {
+        console.log(event)
+        if(event.value == "")
+        {
+            return 'Must be at least 1 character';
+        }
+    },
+    defaultValue: 'This user is a known minor',
+    helpText: 'Default: This user is a known minor'
 },
 {
     type: 'select',
     name: 'modNoteType',
-    label: 'What type of mod note? (Leaving this blank will make a regular mod note)',
+    label: 'What type of mod note?',
     options: [
       {
         label: 'Abuse Warning',
@@ -47,8 +74,18 @@ Devvit.addSettings([
       {
         label: 'Helpful',
         value: 'HELPFUL_USER'
+      },
+      {
+        label: 'Plain Mod Note',
+        value: ''
       }
     ]
+},
+{
+    type: 'boolean',
+    name: 'toggleOnOff',
+    label: "Toggle the app on/off",
+    defaultValue: false,
 }
 ]);
 
@@ -56,44 +93,67 @@ Devvit.addTrigger({
   event: 'PostSubmit', 
   onEvent: async (event, context) => 
   {
-      const { redis } = context;
-    if(event.post.nsfw)
-    {
-        //CHECK IF ON LIST
-        const value = await redis.get(event.author.name);
-        if(value == event.author.name)
-        {
-            //Not allowed to make this nsfw post
-            const reason = await context.settings.get('postReportReason')
-            await context.reddit.report(event.post, {
-                reason: reason,
-            });
-        }
-        
-    }
+      if(await context.settings.get('toggleOnOff'))
+      {
+        PostChecking(event, context)
+      }
 
   },
 });
 
 Devvit.addTrigger({
+    event: 'PostUpdate',
+    onEvent: async (event, context) =>
+    {
+        if(await context.settings.get('toggleOnOff'))
+        {
+            PostChecking(event, context)
+        }
+    },
+});
+
+async function PostChecking(event, context)
+{
+        if(event.post.nsfw)
+        {
+            if(CheckIfOnTheList(event.author.name, context))
+            {
+                const reason = await context.settings.get('postReportReason')
+                await context.reddit.report(event.post, {
+                    reason: reason,
+                });
+            }
+        } 
+}
+
+async function CheckIfOnTheList(key, context)
+{
+    const { redis } = context;
+    const value = await redis.exists(key)
+    return (value >= 1)
+}
+
+Devvit.addTrigger({
   event: 'CommentSubmit', 
   onEvent: async (event, context) => 
   {
-      const { redis } = context;
-
-    if(event.post.nsfw)
+    if(await context.settings.get('toggleOnOff'))
     {
-        const value = await redis.get(event.author.name);
-        if(value == event.author.name)
+
+        if(event.post.nsfw)
         {
-            //Not allowed to make this comment
-            const reason = await context.settings.get('commentReportReason')
-            
-            await context.reddit.report(event.comment, {
-                reason: reason,
-            });
-        }
         
+            if(CheckIfOnTheList(event.author.name, context))
+            {
+                //Not allowed to make this comment
+                const reason = await context.settings.get('commentReportReason')
+            
+                await context.reddit.report(event.comment, {
+                    reason: reason,
+                });
+            }
+        
+        }
     }
   },
 });
@@ -103,29 +163,14 @@ Devvit.addMenuItem({
   location: 'post',
   forUserType: 'moderator',
   onPress: async (event, context) => {
-    
-    const { redis } = context;
-    const post = await context.reddit.getPostById(context.postId);
-    const key = post.authorName;
-    
-    const value = await redis.get(post.authorName);
-    if(value != post.authorName)
+    if(await context.settings.get('toggleOnOff'))
     {
-        
-    
-        const subreddit = await context.reddit.getSubredditById(context.subredditId)
-        const labelType = await context.settings.get('modNoteType')
-        const modNote = await context.reddit.addModNote({
-            redditId: context.postId, 
-            label: labelType.toString(), 
-            note: await context.settings.get('modnoteText'), 
-            subreddit: subreddit.name, 
-            user: post.authorName,
-        })
-        
-        await redis.set(key, modNote.id);
-        
+        const post = await context.reddit.getPostById(context.postId);
+        const key = post.authorName;
+        const returnValue = await AddToList(key, context)
+        AddingToast(returnValue, context, key)
     }
+    
   },
 });
 
@@ -134,35 +179,97 @@ Devvit.addMenuItem({
   location: 'post',
   forUserType: 'moderator',
   onPress: async (event, context) => {
-    
-    const { redis } = context;
-    const subreddit = await context.reddit.getSubredditById(context.subredditId)
-    const post = await context.reddit.getPostById(context.postId);
-    const key = post.authorName;
-    const modNote = await redis.get(key)
-    await context.reddit.deleteModNote({
-            noteId: modNote,
-            subreddit: subreddit.name,
-            user: key
-        });
-    await redis.del(key);
-
+    if(await context.settings.get('toggleOnOff'))
+    {
+        const post = await context.reddit.getPostById(context.postId);
+        const key = post.authorName;
+        const returnValue = await RemoveFromList(key, context)
+        RemovalToast(returnValue, context, key)
+    }
   },
 });
 
-Devvit.addMenuItem({
-  label: 'Add user to list of users not allowed NSFW',
-  location: 'comment',
-  forUserType: 'moderator',
-  onPress: async (event, context) => {
-    
-    const { redis } = context;
-    const comment = await context.reddit.getCommentById(context.commentId);
-    const key = comment.authorName;
-    const value = await redis.get(comment.authorName);
-    if(value != comment.authorName)
+function RemovalToast(returnValue, context, key)
+{
+    if(returnValue == 0)
     {
-        
+       context.ui.showToast('Successfully removed ' + key + ' from the list');
+    }
+    else if(returnValue == -1)
+    {
+       context.ui.showToast('Could not remove ' + key + ' from the list, likely already removed');
+    }
+    else
+    {
+       context.ui.showToast('Error: Unknown Error');
+    }
+}
+
+function AddingToast(returnValue, context, key)
+{
+    if(returnValue == 0)
+    {
+        context.ui.showToast('Successfully added ' + key + ' from the list');
+    }
+    else if(returnValue == -1)
+    {
+        context.ui.showToast('Could not add ' + key + ' from the list, try removing and re-adding to the list');
+    }
+    else
+    {
+        context.ui.showToast('Error: Unknown Error');
+    }
+}
+
+async function RemoveFromList(key, context)
+{
+    const { redis } = context;
+    const subreddit = await context.reddit.getSubredditById(context.subredditId)
+    if(await CheckIfOnTheList(key, context))
+    {
+        try
+        {
+            const modNote = await redis.hGet(key, 'modnoteID')
+            DeleteModNote(modNote, subreddit.name, key, context)
+        }
+        catch (error)
+        {
+            const modNote = await redis.get(key)
+            DeleteModNote(modNote, subreddit.name, key, context)
+        }
+        await redis.del(key);
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
+    return -2;
+    
+}
+
+async function DeleteModNote(modNoteID, subreddit, key, context)
+{
+    try
+    {
+        await context.reddit.deleteModNote({
+            noteId: modNoteID,
+            subreddit: subreddit,
+            user: key
+        });
+    }
+    catch
+    {
+        context.ui.showToast('modnote was not able to be deleted');
+    }
+        return 0;
+}
+
+async function AddToList(key, context)
+{
+    const { redis } = context;
+    if(!await CheckIfOnTheList(key, context))
+    {
         const subreddit = await context.reddit.getSubredditById(context.subredditId)
         const labelType = await context.settings.get('modNoteType')
         const modNote = await context.reddit.addModNote({
@@ -170,9 +277,30 @@ Devvit.addMenuItem({
             label: labelType.toString(), 
             note: await context.settings.get('modnoteText'), 
             subreddit: subreddit.name, 
-            user: comment.authorName,
-         })
-         await redis.set(key, modNote.id);
+            user: key,
+        })
+        await redis.hSet(key, {modnoteID: modNote.id});
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
+    return -2;
+}
+
+
+Devvit.addMenuItem({
+  label: 'Add user to list of users not allowed NSFW',
+  location: 'comment',
+  forUserType: 'moderator',
+  onPress: async (event, context) => {
+    if(await context.settings.get('toggleOnOff'))
+    {
+        const comment = await context.reddit.getCommentById(context.commentId);
+        const key = comment.authorName;
+        const returnValue = await AddToList(key, context)
+        AddingToast(returnValue, context, key)
     }
   },
 });
@@ -182,19 +310,14 @@ Devvit.addMenuItem({
   location: 'comment',
   forUserType: 'moderator',
   onPress: async (event, context) => {
+    if(await context.settings.get('toggleOnOff'))
+    {
+        const comment = await context.reddit.getCommentById(context.commentId);
+        const key = comment.authorName;
     
-    const { redis } = context;
-    const subreddit = await context.reddit.getSubredditById(context.subredditId)
-    const comment = await context.reddit.getCommentById(context.commentId);
-    const key = comment.authorName;
-    const modNote = await redis.get(key)
-    await context.reddit.deleteModNote({
-            noteId: modNote,
-            subreddit: subreddit.name,
-            user: key
-        });
-    await redis.del(key);
-
+        const returnValue = await RemoveFromList(key, context)
+        RemovalToast(returnValue, context, key)
+    }
   },
 });
 
